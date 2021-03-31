e6a85bc0372f1fabc07fa01e13e40ca9f1d35b85
Sam Yeo <yeosamuel95@gmail.com>
2021-03-31 03:05:01 -0600

    Create test for create_deck

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,7 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck can create a list of strings" do
+    assert Cards.create_deck == ["Ace", "Two", "Three"]
   end
 end
