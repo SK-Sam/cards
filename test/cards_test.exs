e5405e61ce781fe495c68445614ce653114fd1ee
Sam Yeo <yeosamuel95@gmail.com>
2021-03-31 03:23:32 -0600

    Add tests for shuffle_deck function

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,15 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "create_deck can create a list of strings" do
+  test "create_deck can create a list of 3 string elements" do
     assert Cards.create_deck == ["Ace", "Two", "Three"]
+  end
+
+  test "shuffle_deck can change the order of the previous deck randomly" do
+    deck = Cards.create_deck
+    shuffled_deck = Cards.shuffle_deck(deck)
+
+    assert shuffled_deck != deck
+    assert Enum.count(shuffled_deck) == Enum.count(deck)
   end
 end
