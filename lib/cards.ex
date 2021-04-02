594f7e92772ce95f90b5360f4dc1328598babf4d
Sam Yeo <yeosamuel95@gmail.com>
2021-04-02 03:59:18 -0600

    Create save, read, and create_hand function

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,13 @@ defmodule Cards do
   @moduledoc """
   Documentation for `Cards`.
   """
+
+  def create_hand(hand_size) do
+    create_deck()
+    |> shuffle_deck
+    |> deal(hand_size)
+  end
+
   def create_deck do
     values = ["Ace", "Two", "Three"]
     suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
@@ -27,5 +34,19 @@ defmodule Cards do
 
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def read(filename) do
+    #{status, binary} = File.read(filename)
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
   end
 end
